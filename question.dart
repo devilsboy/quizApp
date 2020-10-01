88c0e230ffcda463aa18ee1fb2a00f4185d16892
Suneet Jain <44058062+devilsboy@users.noreply.github.com>
2020-10-01 22:21:43 +0530

    Update question.dart

--- question.dart
+++ question.dart
@@ -6,14 +6,6 @@ class Question extends StatelessWidget {
   Question(this.questionText);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      margin: EdgeInsets.all(10),
-      child: Text(
-        questionText,
-        style: TextStyle(fontSize: 28),
-        textAlign: TextAlign.center,
-      ),
-    );
+   // @Kartikay code here
   }
 }
