de69029c09a84ecc8eeeb806b42d1ef63f0c86a3
Suneet Jain <44058062+devilsboy@users.noreply.github.com>
2020-10-01 22:47:00 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -20,18 +20,7 @@ var _totalScore = 0;
 class _MyAppState extends State<MyApp> {
   @override
   void _pressed(int score) {
-    _totalScore += score;
-    // print(_totalScore);
-
-    setState(() {
-      _questionIndex += 1;
-    });
-    print("Answer Chosen!");
-    print(_questionIndex);
-    if (_questionIndex < _questions.length) {
-      print("We have more questions");
-    } else {
-      print("No more questions");
+//Rohan ke dost
     }
   }
 
